548ebe390d210b139b81cca60d97bdf427a86ff7
jclark017 <jclark017@gmail.com>
2020-12-06 11:02:17 -0800

    Daily Update

--- update.bat
+++ update.bat
@@ -1,3 +1,6 @@
+cd ../PowerBI
+del daily.csv
+wget https://covidtracking.com/api/v1/states/daily.csv
 git fetch --all
 git merge remotes/upstream/master
 cd vscode
@@ -6,7 +9,4 @@ cd ../sql
 sqlite3 ../vscode/output.db ".read outputSqlite"
 git commit -am "Daily Update"
 git push
-cd ../PowerBI
-del daily.csv
-wget https://covidtracking.com/api/v1/states/daily.csv
 cd ..
